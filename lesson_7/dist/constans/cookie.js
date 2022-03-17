9633af59647c3d92d7599b48319c17101323910d
ivanys23071996 <ivanys230796@gmail.com>
2022-03-17 17:31:09 +0200

    Access and refresh tokens

--- lesson_7/dist/constans/cookie.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cookie = void 0;
-exports.cookie = {
-    refreshToken: 'refreshToken',
-    maxAge: 24 * 60 * 60 * 1000,
-};
-//# sourceMappingURL=cookie.js.map
