8c327585b3c481495343718c77bed26211f04622
Ivanus Volodymyr <91600635+Ivanus-Volodymyr@users.noreply.github.com>
2022-03-24 16:57:42 +0200

    refresh, user validator (registration, login update)

--- lesson_9/dist/constans/regex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.regex = void 0;
-exports.regex = {
-    email: /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/,
-    phone: /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/,
-    password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,60}$/,
-};
-//# sourceMappingURL=regex.js.map
