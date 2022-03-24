1fe67764a3473fae57fa154352a8c6ea04ba941e
ivanys23071996 <ivanys230796@gmail.com>
2022-03-24 19:43:17 +0200

    ..

--- lesson_8/src/app.ts
+++ lesson_8/src/app.ts
@@ -1,27 +1,27 @@
-import 'reflect-metadata';
-import express from 'express';
-import { createConnection } from 'typeorm';
-
-import { apiRouter } from './routers/apiRoute';
-import { configEnv } from './configs/configEnv';
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.use(apiRouter);
-
-const { port } = configEnv.port;
-app.listen(port, async () => {
-    try {
-        const connection = await createConnection();
-        if (connection) {
-            console.log('Database connected....');
-        }
-    } catch (err) {
-        if (err) {
-            console.log(err);
-        }
-    }
-    console.log(`Server has started on port ${port}..........`);
-});
+// import 'reflect-metadata';
+// import express from 'express';
+// import { createConnection } from 'typeorm';
+//
+// import { apiRouter } from './routers/apiRoute';
+// import { configEnv } from './configs/configEnv';
+//
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded());
+//
+// app.use(apiRouter);
+//
+// const { port } = configEnv.port;
+// app.listen(port, async () => {
+//     try {
+//         const connection = await createConnection();
+//         if (connection) {
+//             console.log('Database connected....');
+//         }
+//     } catch (err) {
+//         if (err) {
+//             console.log(err);
+//         }
+//     }
+//     console.log(`Server has started on port ${port}..........`);
+// });
